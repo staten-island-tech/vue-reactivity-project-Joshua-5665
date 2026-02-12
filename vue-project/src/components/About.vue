fa07b1597d66aa81dbed9d088c9fce0b3be3bc2b
Joshua-5665 <joshuac726@nycstudents.net>
2026-02-12 10:51:48 -0500

    jnjb

--- vue-project/src/components/About.vue
+++ vue-project/src/components/About.vue
@@ -6,8 +6,12 @@
         <h4>Below, you will see the work button. Clicking it once generates US a dollar. Your job is to basically click it 24/7 to make us rich. Easy enough?</h4>
         <h5>Your job starts now</h5>
          <button class="work" @click="increments">Work</button>
-         <button class="work" @click="increments2" v-if="money > 10">Work+1</button>
-        <div class="moneyBox">{{ money }}</div>
+         <button class="work" @click="increments2" v-if="money >= 50">Work+1</button>
+         <button class="work" @click="increments3" v-if="money >= 200">Work+2</button>
+         <button class="work" @click="upgrade" v-if="money >= 200">Farm</button>
+         <button class="work" @click="upgrade2" v-if="money >= 1000">Bakery</button>
+         <button class="work" @click="upgrade3" v-if="money >=2000">Factory</button>
+        <div class="moneyBox"> ${{ money }}</div>
     </div>
 </template>
 
@@ -20,6 +24,38 @@ import { ref } from 'vue'
     
     function increments2() {
         money.value+=2
+    }
+    function increments3() {
+        money.value+=3
+    }
+    
+    const isWorking = ref(false)
+
+    function upgrade() {
+        if (isWorking.value) return
+        money.value-=100
+        isWorking.value = true
+        setInterval(()=> {
+            money.value+=1
+        }, 2000)
+    }
+
+    function upgrade2() {
+        if(isWorking.value) return
+
+        isWorking.value = true
+        setInterval(() => {
+            money.value+=10
+        }, 2000)
+    }
+
+    function upgrade3() {
+        if(isWorking.value) return
+
+        isWorking.value = true
+        setInterval(() => {
+            money.value+=50
+        }, 2000)
     }
 </script>
 
