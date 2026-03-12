233d36ea666aaa4d44dee8fcbd4ff23e82103c62
Joshua-5665 <joshuac726@nycstudents.net>
2026-03-12 13:29:36 -0400

    k

---
+++ vue-project/src/views/HomePage.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="word">
+        <h1>Become a Billionaire</h1>
+        <h2>Your job starts now</h2>
+         <button class="work" @click="increments" @mouseover="colorIn" @mouseout="colorOut">Work</button>
+         <a>Per Second: ${{ perClick }}</a>
+        <div class="moneyBox"> Income: ${{ money }}</div>
+        <RouterLink to="/work">Gamble</RouterLink>
+
+        <Upgrades/>
+        <Information/>
+        <Clickups/>
+        <Extras/>
+    </div>
+</template>
+
+<script setup>
+import Upgrades from '@/components/Upgrades.vue'
+import Information from '@/components/Information.vue'
+import Clickups from '@/components/Clickups.vue'
+import Extras from '@/components/Extras.vue'
+import { click } from '@/store/store.js'
+import {money} from '@/store/store.js'
+import {perClick} from '@/store/store.js'
+
+
+    
+    function increments() {
+        money.value+=click.value
+    }
+
+    function colorIn() {
+        document.querySelector('.work').style.backgroundColor = 'rgb(173, 173, 173)';
+    }
+
+    function colorOut() {
+        document.querySelector('.work').style.backgroundColor = '';
+    }
+
+</script>
+
+<style>
+    .work{
+        height: 50px;
+        width: 100px;
+        margin: 5px;
+        align-items: center;
+        background-color: rgb(196, 196, 196);
+        border-radius: 10px;
+        cursor: pointer;
+    }
+
+    .moneyBox{
+       height: 100px;
+       width: 200px;
+       background-color: rgb(168, 168, 168);
+       margin-top: 20px;
+       margin-left: 5px;
+       align-content: center;
+       justify-self: center;
+    }
+    .word{
+        height: 80vh;
+        width: 100vw; 
+        text-align: center;
+    }
+    a{
+        display: flex;
+        flex-direction: column;
+        font-size: 18px;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+    }
+    body{
+        background-color: rgb(68, 128, 50);
+    }
+    .extrasBox {
+        width: 300px;
+        height: 800px;
+        background-color: gray;
+    }
+</style>
